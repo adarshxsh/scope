c71fe929c1f88fc4b856bed771c7048edf6cc77b
Adarsh <udhabhaw@gmail.com>
2026-09-18 19:20:46 +0000

    Configure R8 minification, resource shrinking, and ProGuard keep rules for Android release build

--- scope/android/build.gradle.kts
+++ scope/android/build.gradle.kts
@@ -19,6 +19,18 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    tasks.withType<JavaCompile>().configureEach {
+        sourceCompatibility = JavaVersion.VERSION_11.toString()
+        targetCompatibility = JavaVersion.VERSION_11.toString()
+    }
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
